b5220bf17b2c55b1401ef80bcbec521d16ebd9d6
AmalAntoney123 <78252452+AmalAntoney123@users.noreply.github.com>
2024-11-05 21:42:59 +0530

    gubnircorn

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from ml_health import HealthPredictor
 import numpy as np
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 predictor = HealthPredictor()
 
 # Initialize the model when starting the server
